3e49f616c5efc3cd1f5d0f0eec9fcf351f99deaf
Kristina Peterson <kripet@amazon.com>
2024-06-26 11:13:26 -0700

    Updated config.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
+import awsAmplify from 'astro-aws-amplify';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
-adapter: awsAmplify(),
-	output: 'server',});
+        adapter: awsAmplify(),
+	output: 'server',
+});
